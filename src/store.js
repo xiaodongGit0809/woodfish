7d9c87ed9916468b4d6c5525670bcdf4b98ecb10
donglongfei <donglongfei@bytedance.com>
2023-12-18 13:07:01 +0800

    feat: 修改打包路径

--- src/store.js
+++ src/store.js
@@ -1,18 +1,13 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
-    state: {       // 准备state——用于存储数据
-        gongde:0
+  state: {
+    gongde: 0,
+  },
+  actions: {},
+  mutations: {
+    gd(state, num) {
+      state.gongde = state.gongde + num;
     },
-
-    actions: {   // 准备actions——用于响应组件中的动作
-    },
-
-
-    mutations: {    // 准备mutations——用于操作数据（state）
-        gd(state,num){
-            state.gongde = state.gongde + num
-        }
-    },
-
-})
+  },
+});
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { resolve } from 'path';
 module.exports = {
-  proxy: {
-    '/api': {
-      target: 'http://127.0.0.1:8000/',
-      changeOrigin: true, // 允许跨域
-    },
-  },
+  base: '/woodfish/',
   plugins: [],
   resolve: {
     alias: {
