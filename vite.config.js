4881c46b0ca1883d11216b09659e8e9cb0a65585
donglongfei <donglongfei@bytedance.com>
2023-12-18 11:30:18 +0800

    feat: 修改打包路径

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
+import { resolve } from 'path';
 module.exports = {
-  proxy:{
-      '/api':{
-          target:'http://127.0.0.1:8000/',
-          changeOrigin:true,  // 允许跨域
-      }
-  }
-
-}
+  proxy: {
+    '/api': {
+      target: 'http://127.0.0.1:8000/',
+      changeOrigin: true, // 允许跨域
+    },
+  },
+  plugins: [],
+  resolve: {
+    alias: {
+      Src: resolve(__dirname, 'src'),
+    },
+  },
+};
