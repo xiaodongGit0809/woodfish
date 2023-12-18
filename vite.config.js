0249686d1ecc4a0026ef9b9e3c7e41d0e0681b8e
donglongfei <donglongfei@bytedance.com>
2023-12-18 14:22:29 +0800

    feat: 更新config文件

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
-import { resolve } from 'path';
+import vue from '@vitejs/plugin-vue';
 module.exports = {
   base: '.',
-  plugins: [],
-  resolve: {
-    alias: {
-      Src: resolve(__dirname, 'src'),
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    cssCodeSplit: true,
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: '[ext]/[name]-[hash].[ext]',
+      },
     },
   },
 };
