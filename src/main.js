c8fba0db5ae84959c8072d1d0d4cf2e0a3e4528d
donglongfei <donglongfei@bytedance.com>
2023-12-18 13:47:29 +0800

    feat: 修改打包路径

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from 'vue';
 import router from './router';
 import store from './store';
-import axios from 'axios';
-import VueAxios from 'vue-axios';
 import ECharts from 'vue-echarts';
 import 'echarts';
 import App from './App.vue';
@@ -14,9 +12,6 @@ import 'element-plus/theme-chalk/index.css';
 createApp(App)
   .use(router)
   .use(store)
-  .use(VueAxios, axios)
   .use(ElementPlus)
   .component('ECharts', ECharts)
   .mount('#app');
-
-axios.defaults.baseURL = 'https://xiaodonggit0809.github.io/';
